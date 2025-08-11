fe97f82a7476ba5dccfbe29ad869046ca36b2531
kenny <2916363651@qq.com>
2025-08-11 15:36:55 +0800

    feat: add a osskey function

--- 216-测试获取ossKey.js
+++ 216-测试获取ossKey.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const getOssKey = (url) => {
   // url合法性
   let parsed;
@@ -6,16 +7,22 @@ const getOssKey = (url) => {
   } catch (e) {
     throw new Error("非法url");
   }
-  console.log(parsed);
   const host = parsed.hostname;
   // 验证是否是阿里云url
   if (!host.endsWith("aliyuncs.com")) {
     throw new Error("不是阿里云 OSS 域名");
   }
   // return url.replace(/^https?:\/\/[^\/]+\//, "");
-  return parsed.pathname;
+  const pathname = parsed.pathname.replace(/^\/(?!$)/, ""); // 只替换第一个 / 且不影响空字符串
+  return pathname;
 };
 const url =
   "https://sh-01-bucket-lvc-dev-private.oss-cn-shanghai.aliyuncs.com/a/b/c/demo.mp4?Expires=1754892385&OSSAccessKeyId=TMP.3KotHr4fiKV4gqrHh7fiETvCHLSysjfaAa15LBBCfXnd4v7ECehYQz6WTqmRX1T7vvUgFUTNxE7WZ53Uaznt1mFYzhCjWt&Signature=Kv8SJ9pMpKQx1pwL%2BFquhj%2FMy4o%3D";
 const ossKey = getOssKey(url);
 console.log(ossKey);
+
+let fileExtension = path.extname("/a/b/c.png"); // ".jpg"
+console.log("fileExtension:", fileExtension);
+
+const pathname = "/dsfds/fdsfdsf".replace(/^\/(?!$)/, ""); // 只替换第一个 / 且不影响空字符串
+console.log(pathname);
